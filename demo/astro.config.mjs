49a23c2addd1722aa6791adff0ce25ee476128c4
Jack Fletcher <jackowenfletcher@gmail.com>
2023-03-24 02:19:21 +0000

    Add tailwind to demo

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 import cooklang from 'astro-cooklang';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [cooklang()],
+	integrations: [
+		cooklang(),
+		tailwind()
+	]
 });
