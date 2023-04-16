8584f022a3201e162d5cc03434756533f53937ec
Jack Fletcher <jackowenfletcher@gmail.com>
2023-04-16 14:22:21 +0000

    Semi-working recipe output module

--- packages/cooklang/src/index.ts
+++ packages/cooklang/src/index.ts
@@ -42,64 +42,104 @@ export default function cooklangIntegration(
         const { updateConfig, addContentEntryType, addPageExtension } =
           params as SetupHookParams;
 
-        /**
-         *
-         */
-        function getEntryInfo({
-          fileUrl,
-          contents,
-        }: {
-          fileUrl: URL;
-          contents: string;
-        }) {
-          // Parse the recipe...
-          const recipe = new Recipe(contents);
+/**
+ *
+ */
+function getEntryInfo({
+  fileUrl,
+  contents,
+}: {
+  fileUrl: URL;
+  contents: string;
+}) {
+  // Parse the recipe...
+  const recipe = new Recipe(contents);
 
-          // Extract parts.
-          const { ingredients, cookwares, metadata, steps, shoppingList } =
-            recipe;
+  // Extract parts.
+  const { ingredients, cookwares, metadata, steps, shoppingList } = recipe;
 
-          // TODO: Assemble parts into human-readable format and render (as Markdown?)
-          // We'll just use the raw file contents for now.
-          const body = contents;
+  // TODO: Assemble parts into human-readable format and render (as Markdown?)
+  // We'll just use the raw file contents for now.
+  const body = contents;
 
-          return {
-            data: {
-              ingredients,
-              cookwares,
-              metadata,
-              steps,
-              shoppingList,
-            },
-            body,
-            slug: metadata.slug,
-            rawData: "",
-          };
-        }
+  return {
+    data: {
+      ingredients,
+      cookwares,
+      metadata,
+      steps,
+      shoppingList,
+    },
+    body,
+    slug: metadata.slug,
+    rawData: "",
+  };
+}
 
-        /**
-         *
-         */
-        async function getRenderModule({
-          viteId,
-          entry,
-        }: {
-          entry: ContentEntryModule;
-          viteId: string;
-        }) {
-          console.log({ viteId, entry });
+/**
+ *
+ */
+async function getRenderModule({
+  viteId,
+  entry,
+}: {
+  entry: ContentEntryModule;
+  viteId: string;
+}) {
+  const { body } = entry;
+  // const { steps, cookwares, ingredients } = data
+  // console.log({ viteId, entry });
 
-          return {
-            code: `
-const recipeSource = ${JSON.stringify(entry.body, null, 4)}
+  const code = `
+          import { jsx as h } from "astro/jsx-runtime";
 
-console.log({recipeSource})
+          const recipeSource = ${JSON.stringify(body, null, 4)}
 
-export async function Content (props) {
-  return recipeSource
-}`,
-          };
-        }
+          export async function Content (props) {
+            return h('div', { children: [recipeSource] });
+          }
+
+          export default Content
+          `;
+
+  return {
+    code,
+    vite: {
+      lang: "ts",
+    },
+  };
+}
+
+/**
+ *
+ */
+const contentTypesTemplate = `
+declare module 'astro:content' {
+  interface Render {
+    '.cook': Promise<{
+      Content(props: Record<string, any>): import('astro').MarkdownInstance<{}>['Content'];
+    }>;
+  }
+}`;
+
+export default function cooklangIntegration(
+  inputConfig: AstroCooklangConfig = {}
+): AstroIntegration {
+  return {
+    name: "@astrojs/cooklang",
+    hooks: {
+      "astro:config:setup": async (params) => {
+        const { updateConfig, addContentEntryType, addPageExtension } =
+          params as SetupHookParams;
+
+        // Register Vite plugin...
+        // Using my vite plugin for now, but we probably want a custom
+        // transformer later.
+        // const viteConfig: InlineConfig = {
+        //   plugins: [cooklangVite()],
+        // };
+
+        // updateConfig({ vite: viteConfig });
 
         // TODO: Try disabling.
         addPageExtension(".cook");
@@ -107,18 +147,9 @@ export async function Content (props) {
         addContentEntryType({
           extensions: [".cook"],
           getEntryInfo,
-          // TODO...
           getRenderModule,
           contentModuleTypes: contentTypesTemplate,
         });
-
-        // Using my vite plugin for now, but we probably want a custom
-        // transformer later.
-        const viteConfig: InlineConfig = {
-          plugins: [cooklangVite()],
-        };
-
-        updateConfig({ vite: viteConfig });
       },
     },
   };
