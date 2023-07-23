2ba86cafca31f500f535b9d0b6ac99203f361455
Jack Fletcher <jackowenfletcher@gmail.com>
2023-07-23 12:27:38 +0000

    Add base url env

--- demo/astro.config.ts
+++ demo/astro.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import cooklang from "astro-cooklang";
 
-const baseUrl = import.meta.env.PUBLIC_BASE_URL as string || undefined;
+const baseUrl = import.meta.env.VITE_BASE_URL as string || undefined;
+
+console.log({baseUrl, env: import.meta.env})
 
 // https://astro.build/config
 export default defineConfig({
