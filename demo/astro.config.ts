6a4699847087287145b04e91e241a7359df4c257
Jack Fletcher <jackowenfletcher@gmail.com>
2023-07-23 15:27:32 +0000

    tidy formatting

--- demo/astro.config.ts
+++ demo/astro.config.ts
@@ -5,7 +5,7 @@ import { never } from "astro/zod";
 
 const baseUrlPath = import.meta.env.VITE_BASE_PATH as string;
 
-console.log({baseUrlPath, env: import.meta.env})
+console.log({ baseUrlPath, env: import.meta.env });
 
 // https://astro.build/config
 export default defineConfig({
