8ee5c26b8479a9281e87c239d1ff101d2e859130
Jack Fletcher <jackowenfletcher@gmail.com>
2023-07-23 14:41:33 +0000

    fix the base path for good this time?

--- demo/astro.config.ts
+++ demo/astro.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import cooklang from "astro-cooklang";
+import { never } from "astro/zod";
 
-const baseUrlPath = import.meta.env.PUBLIC_BASE_PATH as string;
+const baseUrlPath = import.meta.env.VITE_BASE_PATH as string;
 
 console.log({baseUrlPath, env: import.meta.env})
 
 // https://astro.build/config
 export default defineConfig({
   base: baseUrlPath,
-  trailingSlash: 'never',
 
   integrations: [
     cooklang(),
