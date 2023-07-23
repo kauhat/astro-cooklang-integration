01de48db11023f99320b8429cfd40a0a5d13428b
Jack Fletcher <jackowenfletcher@gmail.com>
2023-07-23 10:43:39 +0000

    Source BASE_URL from env

--- demo/astro.config.ts
+++ demo/astro.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import cooklang from "astro-cooklang";
 
+const baseUrl = import.meta.env.PUBLIC_BASE_URL as string || undefined;
+
 // https://astro.build/config
 export default defineConfig({
+  base: baseUrl,
   integrations: [
     cooklang(),
     tailwind({ config: { path: "tailwind.config.cjs" } }),
