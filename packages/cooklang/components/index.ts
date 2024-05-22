20e1f0c73b8ce6c5ce4f331b52edd93bda6977c0
Jack Fletcher <jackowenfletcher@gmail.com>
2024-05-22 20:22:46 +0000

    Move integration components

---
+++ packages/cooklang/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Renderer } from "./Renderer.astro";
+export { default as RecipeIngredients } from "./RecipeIngredients.astro";
+export { default as RecipeSteps } from "./RecipeSteps.astro";
